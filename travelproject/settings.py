620b4fb5419c71a4a76c3e7cd7116ce36bc95da3
Sarang S <sarangsvkm@gmail.com>
2022-11-01 20:04:13 +0530

    travel

--- travelproject/settings.py
+++ travelproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hl)i)*brwuue$g=dt13_vp4%m5z2=#3uip$t0$f(5rel)cuoj#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
